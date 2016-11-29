db0982e822406f8afad02cf372b7b9f51d7cc9e4
bitpshr <paul@paulbouchon.net>
2016-11-29 13:22:59 -0500

    Exclude module stylus files

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -35,8 +35,8 @@ module.exports = {
 			{ test: /\.js?$/, loader: 'umd-compat-loader' },
 			{ test: /\.html$/, loader: 'html' },
 			{ test: /\.(jpe|jpg|png|woff|woff2|eot|ttf|svg)(\?.*$|$)/, loader: 'file?name=[path][name].[hash:6].[ext]' },
-			{ test: /src[\\\/]main\.styl$/, loader: ExtractTextPlugin.extract(['css-loader?sourceMap', 'stylus-loader']) },
-			{ test: /\.module\.styl$/, loader: 'style-loader!css-loader?modules!stylus-loader' },
+			{ test: /\.styl$/, exclude: /\.module\.styl$/, loader: ExtractTextPlugin.extract(['css-loader?sourceMap', 'stylus-loader']) },
+			{ test: /\.module\.styl$/, loader: 'style-loader!css-loader?modules!stylus-loader' }
 		]
 	},
 	plugins: [
