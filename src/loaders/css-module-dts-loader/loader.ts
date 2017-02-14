a078b155f68784c0d90b7ba8ecf04f25d2cd308a
Tom Dye <github@tomdye.co.uk>
2017-02-14 11:20:38 +0000

    got a cyclical dependency and infinite rebuilds

--- src/loaders/css-module-dts-loader/loader.ts
+++ src/loaders/css-module-dts-loader/loader.ts
@@ -1,47 +1,83 @@
 import * as ts from 'typescript';
+// import { readFileSync } from 'fs';
+
 import { resolve, dirname } from 'path';
 const DtsCreator = require('typed-css-modules');
+const { parseQuery } = require('loader-utils');
 const creator: any = new DtsCreator({
 	camelCase: true
 });
 
-async function generateDTSFile(filePath: string): Promise<void> {
+async function generateDTSFile(filePath: string, filePaths: string[]) {
 	const content = await creator.create(filePath);
 	await content.writeFile();
+	console.log('PRELOADER -> written DTS' + filePath);
+	// filePaths.push(filePath + '.d.ts');
 }
 
-async function checkNodeForCSSImport(node: ts.Node): Promise<void> {
+async function checkNodeForCSSImport(node: ts.Node, filePaths: string[]): Promise<string | void> {
 	if (node.kind === ts.SyntaxKind.StringLiteral) {
 		const importPath = node.getText().replace(/\'|\"/g, '');
 		if (/.css$/.test(importPath)) {
 			const parentFileName = node.getSourceFile().fileName;
 			const absoluteCssFileName = resolve(dirname(parentFileName), importPath);
-			await generateDTSFile(absoluteCssFileName);
+			return await generateDTSFile(absoluteCssFileName, filePaths);
 		}
 	}
 }
 
-async function checkNode(node: ts.Node): Promise<any> {
-	const promises: Promise<void | void[]>[] = [];
+async function checkNode(node: ts.Node, filePaths: string[]): Promise<any> {
+	const promises: Promise<any | any[]>[] = [];
 	switch (node.kind) {
 		case ts.SyntaxKind.SourceFile:
 			ts.forEachChild(node, (childNode: ts.Node) => {
-				promises.push(checkNode(childNode));
+				promises.push(checkNode(childNode, filePaths));
 			});
 			break;
 		case ts.SyntaxKind.ImportDeclaration:
 			ts.forEachChild(node, (childNode: ts.Node) => {
-				promises.push(checkNodeForCSSImport(childNode));
+				promises.push(checkNodeForCSSImport(childNode, filePaths));
 			});
 			break;
 	}
 
-	return Promise.all(promises);
+	await Promise.all(promises);
 }
+
+async function findCSSImports(sourceFile: ts.Node): Promise<any> {
+	const filePaths: string[] = [];
+	await checkNode(sourceFile, filePaths);
+	return filePaths;
+}
+
+const fileType = {
+	css: 'css',
+	ts: 'ts'
+};
 
 export default async function (this: any, content: string, sourceMap?: any): Promise<void> {
 	const callback = this.async();
-	let sourceFile = ts.createSourceFile(this.resourcePath, content, ts.ScriptTarget.Latest, true);
-	await checkNode(sourceFile);
+	const { type = fileType.ts }: { type: string } = parseQuery(this.query);
+	let filePaths: string[] = [];
+
+	switch (type) {
+		case fileType.css:
+			console.log('PRELOADER -> CSS: ' + this.resourcePath);
+			generateDTSFile(this.resourcePath, filePaths);
+			// this.addDependency(this.resourcePath + '.d.ts');
+
+			break;
+		case fileType.ts:
+			const sourceFile = ts.createSourceFile(this.resourcePath, content, ts.ScriptTarget.Latest, true);
+			console.log('PRELOADER -> TS: ' + this.resourcePath);
+			filePaths = await findCSSImports(sourceFile);
+
+			filePaths.forEach((filePath) => {
+				this.addDependency(filePath);
+			});
+
+			break;
+	}
+
 	callback(null, content, sourceMap);
 }
