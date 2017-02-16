24303389c5778eca7d7e5ab8066bf03dd2672a0d
Tom Dye <github@tomdye.co.uk>
2017-02-16 13:48:29 +0000

    added emit

--- src/loaders/css-module-dts-loader/loader.ts
+++ src/loaders/css-module-dts-loader/loader.ts
@@ -8,16 +8,20 @@ const creator: any = new DtsCreator({
 	camelCase: true
 });
 
-async function generateDTSFile(filePath: string, filePaths: string[]) {
-	const content = await creator.create(filePath);
-	await content.writeFile();
-	// console.log('PRELOADER -> written DTS' + filePath);
-
-	// emit file here
-	filePaths.push(filePath + '.d.ts');
+type DTSFileData = {
+	name: string;
+	content: string;
 }
 
-async function checkNodeForCSSImport(node: ts.Node, filePaths: string[]): Promise<string | void> {
+async function generateDTSFile(filePath: string, filePaths: DTSFileData[]) {
+	const content = await creator.create(filePath);
+	await content.writeFile();
+	console.log('PRELOADER -> written DTS' + filePath);
+
+	filePaths.push({ name: filePath + '.d.ts', content: content.formatted });
+}
+
+async function checkNodeForCSSImport(node: ts.Node, filePaths: DTSFileData[]): Promise<string | void> {
 	if (node.kind === ts.SyntaxKind.StringLiteral) {
 		const importPath = node.getText().replace(/\'|\"/g, '');
 		if (/.css$/.test(importPath)) {
@@ -28,7 +32,7 @@ async function checkNodeForCSSImport(node: ts.Node, filePaths: string[]): Promis
 	}
 }
 
-async function checkNode(node: ts.Node, filePaths: string[]): Promise<any> {
+async function checkNode(node: ts.Node, filePaths: DTSFileData[]): Promise<any> {
 	const promises: Promise<any | any[]>[] = [];
 	switch (node.kind) {
 		case ts.SyntaxKind.SourceFile:
@@ -47,7 +51,7 @@ async function checkNode(node: ts.Node, filePaths: string[]): Promise<any> {
 }
 
 async function findCSSImports(sourceFile: ts.Node): Promise<any> {
-	const filePaths: string[] = [];
+	const filePaths: DTSFileData[] = [];
 	await checkNode(sourceFile, filePaths);
 	return filePaths;
 }
@@ -60,24 +64,28 @@ const fileType = {
 export default async function (this: any, content: string, sourceMap?: any): Promise<void> {
 	const callback = this.async();
 	const { type = fileType.ts }: { type: string } = parseQuery(this.query);
-	let filePaths: string[] = [];
+	let filePaths: DTSFileData[] = [];
 
 	switch (type) {
 		case fileType.css:
 			console.log('PRELOADER -> CSS: ' + this.resourcePath);
 			// generateDTSFile(this.resourcePath, filePaths);
-			this.addDependency(this.resourcePath + '.d.ts');
+			// this.addDependency(this.resourcePath + '.d.ts');
 
 			break;
 		case fileType.ts:
 			const sourceFile = ts.createSourceFile(this.resourcePath, content, ts.ScriptTarget.Latest, true);
 			console.log('PRELOADER -> TS: ' + this.resourcePath);
 			filePaths = await findCSSImports(sourceFile);
-
-			filePaths.forEach((filePath) => {
+			filePaths.forEach(({ name, content }) => {
 				// check modified date
 				// if css file not cached and not modified don't addDependency
-				this.addDependency(filePath);
+
+				// emit file
+				this.emitFile(name, content);
+
+				this.addDependency(name);
+
 			});
 
 			break;
