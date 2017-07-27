b6df5a3ca051d224c9795f5dd4d4de4bd9413e20
Bradley Maier <bmaier@sitepen.com>
2017-07-27 09:23:41 -0400

    Remove unused module and streamline guard

--- src/configureLoader.ts
+++ src/configureLoader.ts
@@ -1,9 +1,7 @@
 import global from '@dojo/shim/global';
 import '@dojo/shim/Promise';
 
-if (!global.dojo2ExternalLoaderConfigPromise) {
-	global.dojo2ExternalLoaderConfigPromise = Promise.resolve();
-}
+global.dojo2ExternalLoaderConfigPromise = global.dojo2ExternalLoaderConfigPromise || Promise.resolve();
 
 export default function configureLoader(loaderConfigCallback: () => Promise<void>) {
 	global.dojo2ExternalLoaderConfigPromise = loaderConfigCallback();
